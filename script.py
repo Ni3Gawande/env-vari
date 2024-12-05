5540f9caeb18c034240313a1112844de2ff46766
Lucifer <knitin.gawande95@gmail.com>
2024-12-05 13:14:16 +0530

    testing

--- script.py
+++ script.py
@@ -5,19 +5,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
+import pytest
 
-load_dotenv()
-driver = webdriver.Chrome()
-driver.get("https://katalon-demo-cura.herokuapp.com/")
-wait = WebDriverWait(driver, 10)
-driver.maximize_window()
-wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[normalize-space()='Make Appointment'])[1]"))).click()
-username = wait.until(EC.element_to_be_clickable((By.ID, "txt-username")))
-username.click()
-username.send_keys(os.getenv("name"))
-passw = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='txt-password']")))
-passw.click()
-passw.send_keys(os.getenv("password"))
-wait.until(EC.element_to_be_clickable((By.ID, "btn-login"))).click()
-time.sleep(4)
-driver.close()
+def test_login():
+        load_dotenv()
+        driver = webdriver.Chrome()
+        driver.get("https://katalon-demo-cura.herokuapp.com/")
+        wait = WebDriverWait(driver, 10)
+        driver.maximize_window()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[normalize-space()='Make Appointment'])[1]"))).click()
+        username = wait.until(EC.element_to_be_clickable((By.ID, "txt-username")))
+        username.click()
+        username.send_keys(os.getenv("name"))
+        passw = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='txt-password']")))
+        passw.click()
+        passw.send_keys(os.getenv("password"))
+        wait.until(EC.element_to_be_clickable((By.ID, "btn-login"))).click()
+        title=driver.title
+        print(title)
+        assert title == "CURA Healthcare Service"
+        driver.close()
